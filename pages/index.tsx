f39e038bf167a5679a99ce6ed2ac238c1a2138a6
Sam Selikoff <sam.selikoff@gmail.com>
2022-08-05 13:04:24 -0400

    End of video

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,156 +1,113 @@
-import { useIsPresent, motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
 
-const peopleSeed = [
-  {
-    id: 1,
-    name: "Person 1",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  {
-    id: 2,
-    name: "Person 2",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  {
-    id: 3,
-    name: "Person 3",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  {
-    id: 4,
-    name: "Person 4",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-  {
-    id: 5,
-    name: "Person 5",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
-  },
-];
+let x = 3;
+export default function () {
+  const [items, setItems] = useState([1, 2, 3]);
+  const [toggle, setToggle] = useState(false);
 
-export default function HomePage() {
-  let [people, setPeople] = useState(peopleSeed);
+  useEffect(() => {
+    window.toggle = () => setToggle((toggle) => !toggle);
+  }, []);
 
-  function deletePerson(id: number) {
-    setPeople((people) => people.filter((p) => p.id !== id));
+  function addItem() {
+    x++;
+    setItems((items) => [...items, x]);
+  }
+
+  function removeItem(item: any) {
+    setItems((items) => items.filter((i) => i !== item));
   }
 
   return (
-    <div className="mt-8 px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Users</h1>
-          <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their name, title,
-            email and role.
-          </p>
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-          >
-            Add user
-          </button>
-        </div>
+    <div className="p-20">
+      <div className="">
+        <button onClick={addItem} className="border rounded px-2 py-1">
+          Add
+        </button>
       </div>
-      <div className="mt-8 flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table
-                cellPadding={0}
-                className="min-w-full divide-y divide-gray-300 relative"
-              >
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Title
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Email
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Role
-                    </th>
-                    <th />
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-200 bg-white">
-                  <AnimatePresence>
-                    {people.map((person) => (
-                      <TR
-                        person={person}
-                        onClick={() => deletePerson(person.id)}
-                        key={person.id}
-                      />
-                    ))}
-                  </AnimatePresence>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
+
+      {/* <table cellPadding={0} className="mt-8 w-full">
+        <thead>
+          <tr>
+            <th>Col 1</th>
+            <th>Col 2</th>
+            <th>Col 3</th>
+          </tr>
+        </thead>
+        <tbody className="relative">
+          <AnimatePresence>
+            {items.map((item) => (
+              <TR key={item} item={item} removeItem={removeItem} />
+            ))}
+          </AnimatePresence>
+        </tbody>
+      </table> */}
+      <ul className="mt-8 border rounded p-8 overflow-hidden">
+        <AnimatePresence initial={false}>
+          {items.map((item) => (
+            <motion.li
+              key={item}
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "auto" }}
+              exit={{ opacity: 0, height: 0 }}
+              transition={{
+                opacity: { duration: 0.3 },
+                height: { duration: 0.4 },
+              }}
+            >
+              <div className="flex items-center justify-between py-2 border-b">
+                <span>Item {item}</span>
+                <button
+                  onClick={() => removeItem(item)}
+                  className="border rounded w-8 h-8"
+                >
+                  &times;
+                </button>
+              </div>
+            </motion.li>
+          ))}
+        </AnimatePresence>
+      </ul>
+      {/* <div className="mt-8">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt,
+          cum in laudantium atque est eligendi laboriosam assumenda, officia
+          praesentium sunt temporibus. Facilis mollitia modi debitis consectetur
+          eaque quia, explicabo aliquid!
+        </p>
+      </div> */}
     </div>
   );
 }
 
-function TR({ person, onClick }) {
+function TR({ item, removeItem }) {
   let isPresent = useIsPresent();
 
   return (
     <motion.tr
       layout
-      transition={{ layout: { type: "tween" } }}
-      exit={{ opacity: 0 }}
-      style={{ position: isPresent ? "relative" : "absolute" }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{
+        opacity: 0,
+      }}
+      transition={{ opacity: { duration: 0.2 } }}
+      style={{
+        position: isPresent ? "relative" : "absolute",
+        display: isPresent ? "table-row" : "flex",
+        alignItems: isPresent ? "" : "center",
+      }}
       className="w-full"
     >
-      <td className="w-[20%] whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-        {person.name}
-      </td>
-      <td className="w-[20%] whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-        {person.title}
-      </td>
-      <td className="w-[20%] whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-        {person.email}
-      </td>
-      <td className="w-[20%] whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-        {person.role}
-      </td>
-      <td className="w-[20%] relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+      <td className="w-1/3">1: item {item}</td>
+      <td className="w-1/3">2: item {item === 2 ? "Lorem" : item}</td>
+      <td className="w-1/3 text-center">
         <button
-          onClick={onClick}
-          className="text-indigo-600 hover:text-indigo-900"
+          onClick={() => removeItem(item)}
+          className="w-8 h-8 border rounded"
         >
-          Delete<span className="sr-only">, {person.name}</span>
+          &times;
         </button>
       </td>
     </motion.tr>
